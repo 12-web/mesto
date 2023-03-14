9938e7ed5d11f5af242d866714f39421a8c05b13
Anatoliy Chudnov <tolik.chudnov@gmail.com>
2023-03-14 10:51:19 +0300

    fix: исправить замечания ревьюера

--- script/index.js
+++ script/index.js
@@ -1,80 +1,45 @@
-// ------------перевод иконки сердца в активное состояние---------------
-let likeButtons = document.querySelectorAll('.journey__like-btn');
-
-// проверка наличия массива
-if(likeButtons.length > 0) {
-
-  likeButtons.forEach(function(item) {
-
-    item.addEventListener('click', function() {
-      item.firstElementChild.classList.toggle('journey__like-img_active');
-    });
-
-  });
-
-  let likeForms = document.querySelectorAll('.journey__like-form');
-
-  // отправка формы активации сердца
-  // на данный момент - отмена перезагрузки страницы
-  likeForms.forEach(function(item) {
-
-    item.addEventListener('submit', function(e) {
-      e.preventDefault();
-    });
-
-  });
-}
-// ------------------------------------------------------------------------------
-
-// ---------------------перевод попапа в активное состояние----------------------
 const popup = document.querySelector('.popup');
 const popupCloseBtn = document.querySelector('.popup__close-btn');
 const formElement = document.querySelector('.popup__form');
-const userNameInput = document.querySelector('.popup__name');
-const jobInput = document.querySelector('.popup__profession');
+const userNameInput = document.querySelector('.popup_value_name');
+const jobInput = document.querySelector('.popup_value_profession');
 
-const profile = document.querySelector('.profile');
 const userName = document.querySelector('.profile__name');
 const profession = document.querySelector('.profile__profession');
+const profileEditBtn = document.querySelector('.profile__edit-btn');
 
-// проверка наличия попапа
-if(popup) {
+// функция открытия попапа
+function openPopup () {
+  popup.classList.add('popup_opened');
 
   // присвоение полям ввода значений из текста
   userNameInput.value = userName.textContent;
   jobInput.value = profession.textContent;
-
-  // функция загрытия попапа
-  function closePopup () {
-    popup.classList.remove('popup_opened');
-  }
-
-  // закрытие попапа
-  popupCloseBtn.addEventListener('click', closePopup);
-
-
-  const profileEditBtn = document.querySelector('.profile__edit-btn');
-
-  // открытие попапа
-  profileEditBtn.addEventListener('click', function() {
-    popup.classList.add('popup_opened');
-  });
-
-  // функция изменения данных профиля
-  function handleFormSubmit (e) {
-      e.preventDefault();
-
-      let userNameInputValue = userNameInput.value;
-      let jobInputValue = jobInput.value;
-
-      userName.textContent = userNameInputValue;
-      profession.textContent = jobInputValue;
-
-      closePopup();
-  }
-
-  // отправка формы редактирования данных профиля
-  formElement.addEventListener('submit', handleFormSubmit);
 }
-// ------------------------------------------------------------------------------
 
+// функция загрытия попапа
+function closePopup () {
+  popup.classList.remove('popup_opened');
+}
+
+// функция изменения данных профиля
+function handleFormSubmit (e) {
+  e.preventDefault();
+
+  let userNameInputValue = userNameInput.value;
+  let jobInputValue = jobInput.value;
+
+  userName.textContent = userNameInputValue;
+  profession.textContent = jobInputValue;
+
+  closePopup();
+}
+
+// открытие попапа
+profileEditBtn.addEventListener('click', openPopup);
+
+// закрытие попапа
+popupCloseBtn.addEventListener('click', closePopup);
+
+// отправка формы редактирования данных профиля
+formElement.addEventListener('submit', handleFormSubmit);
